ec4523b9a27a5ca5fb3b693441d81d68b1468f53
Mangjun <myoungjun55@naver.com>
2025-04-08 15:18:03 +0900

    Modify : 보너스 과제 추가 및 리펙터링

--- project1/procedure1/problem7/mars_mission_computer.py
+++ project1/procedure1/problem7/mars_mission_computer.py
@@ -3,87 +3,122 @@
 from procedure1.problem6.mars_mission_computer import DummySensor
 import time
 import threading
+
+# 문제 8
 import platform
 import psutil # pip install psutil
 
+# 전역 변수
+stop_program = False
+standard_time = (300, "5분")
+keys = (
+    'mars_base_internal_temperature',
+    'mars_base_external_temperature',
+    'mars_base_internal_humidity',
+    'mars_base_external_illuminance',
+    'mars_base_internal_co2',
+    'mars_base_internal_oxygen'
+)
+system_exit = ('q', 'Q')
+
+# Json 형식 출력
+def print_json(data) :
+    data_length = len(data)
+    print()
+    print('{')
+    for index, (key, value) in enumerate(data.items()) :
+        if index == data_length - 1 :
+            print('    "{0}": {1}'.format(key, value))
+        else :
+            print('    "{0}": {1},'.format(key, value))
+    print('}')
+    print()
+
 class MissonComputer :
     def __init__(self):
-        self.env_values = dict()
-        self.env_values['mars_base_internal_temperature'] = 0
-        self.env_values['mars_base_external_temperature'] = 0
-        self.env_values['mars_base_internal_humidity'] = 0
-        self.env_values['mars_base_external_illuminance'] = 0
-        self.env_values['mars_base_internal_co2'] = 0
-        self.env_values['mars_base_internal_oxygen'] = 0
+        self.env_values = {key: 0 for key in keys}
     
     def get_sensor_data(self, sensor) :
         sensor.set_env()
         sensor_data = sensor.get_env()
-        self.env_values['mars_base_internal_temperature'] = sensor_data['mars_base_internal_temperature']
-        self.env_values['mars_base_external_temperature'] = sensor_data['mars_base_external_temperature']
-        self.env_values['mars_base_internal_humidity'] = sensor_data['mars_base_internal_humidity']
-        self.env_values['mars_base_external_illuminance'] = sensor_data['mars_base_external_illuminance']
-        self.env_values['mars_base_internal_co2'] = sensor_data['mars_base_internal_co2']
-        self.env_values['mars_base_internal_oxygen'] = sensor_data['mars_base_internal_oxygen']
+
+        for key in keys:
+            self.env_values[key] = sensor_data[key]
 
         return self.env_values
 
-    def get_mission_computer_info(self) :
+    # 문제 8
+    def get_mission_computer_info(self, setting, error) :
         try :
-            os = platform.platform()
-            os_version = platform.version()
-            cpu = platform.processor()
-            cpu_core_count = psutil.cpu_count(logical=False)
-            mem = psutil.virtual_memory()
-            print('{')
-            print('    "운영체제": "{0}",'.format(os))
-            print('    "운영체제 버전": "{0}",'.format(os_version))
-            print('    "CPU의 타입": "{0}",'.format(cpu))
-            print('    "CPU의 코어 수": "{0}",'.format(cpu_core_count))
-            print('    "메모리의 크기": "{0:.2f}GB"'.format(mem.total / (1024**3)))
-            print('}')
-        except :
-            print('운영체제 정보를 가져오는데 실패하였습니다.')
+            os_data = {}
+
+            if 'os' in setting :
+                os_data['운영체제'] = platform.platform()
             
-    def get_mission_computer_load(self) :
-        try :
-            cpu_current_using_percent = psutil.cpu_percent()
-            mem_current_using_precent = psutil.virtual_memory().percent
-            print('{')
-            print('    "CPU 실시간 사용량": "{0}%",'.format(cpu_current_using_percent))
-            print('    "메모리 실시간 사용량": "{0}%"'.format(mem_current_using_precent))
-            print('}')
+            if 'os_version' in setting :
+                os_data['운영체제 버전'] = platform.version()
+
+            if 'cpu' in setting :
+                os_data['CPU의 타입'] = platform.processor()
+
+            if 'cpu_core_count' in setting :
+                os_data['CPU의 코어 수'] = psutil.cpu_count(logical=False)
+
+            if 'mem' in setting :
+                os_data['메모리의 크기'] = psutil.virtual_memory()
+
+            if len(os_data) != 0 :
+                print_json(os_data)
         except :
-            print('CPU 및 메모리 사용량을 가져오는데 실패하였습니다.')
+            print(error)
+            
+    def get_mission_computer_load(self, setting, error) :
+        try :
+            using_percent_data = {}
+
+            if 'cpu_using_percent' in setting :
+                using_percent_data['CPU 실시간 사용량'] = psutil.cpu_percent()
+
+            if 'mem_using_percent' in setting :
+                using_percent_data['메모리 실시간 사용량'] = psutil.virtual_memory().percent
+
+            if len(using_percent_data) != 0 :
+                print_json(using_percent_data)
+            
+        except :
+            print(error)
     
+
+# 쓰레드 키보드 입력 처리
 def get_user_input():
     global stop_program
     while True:
         user_input = input()
-        if user_input == 'q' or user_input == 'Q':
+        if user_input in system_exit:
             print("System stopped...")
             stop_program = True
-            break
 
-stop_program = False
-
-input_thread = threading.Thread(target=get_user_input)
-input_thread.daemon = True
-input_thread.start()
+# 평균 구하기
+def get_avg(env_values, cnt) :
+    if cnt == 0:
+        return {key: 0 for key in keys}
+    else :
+        return {key: env_values[key] / cnt for key in keys}
 
 if __name__ == '__main__':
     ds = DummySensor()
     RunComputer = MissonComputer()
 
-    avg_env = dict()
-    cnt = 0
+    # 쓰레드 생성
+    input_thread = threading.Thread(target=get_user_input)
+    input_thread.daemon = True
+    input_thread.start()
 
-    avg_env['mars_base_internal_temperature'] = 0
-    avg_env['mars_base_external_temperature'] = 0
-    avg_env['mars_base_internal_humidity'] = 0
-    avg_env['mars_base_external_illuminance'] = 0
-    avg_env['mars_base_internal_co2'] = 0
-    avg_env['mars_base_internal_oxygen'] = 0
+    # 평균 환경 데이터 초기화
+    avg_env = {key: 0 for key in keys}
+
+    # 평균 환경 데이터 측정 카운트 초기화
+    cnt = 0
 
     start_time = time.time()
 
@@ -91,46 +126,27 @@ if __name__ == '__main__':
         current_time = time.time()
         elsapsed_time = current_time - start_time
 
-        if elsapsed_time >= 300 :
-            print("---5분 동안의 평균 환경 데이터---")
-            print('{')
-            for key, value in avg_env.items() :
-                if key == 'mars_base_internal_oxygen' :
-                    print('    "{0}": {1}'.format(key, value/cnt))
-                else :
-                    print('    "{0}": {1},'.format(key, value/cnt))
-            print('}')
-            print()
-            avg_env['mars_base_internal_temperature'] = 0
-            avg_env['mars_base_external_temperature'] = 0
-            avg_env['mars_base_internal_humidity'] = 0
-            avg_env['mars_base_external_illuminance'] = 0
-            avg_env['mars_base_internal_co2'] = 0
-            avg_env['mars_base_internal_oxygen'] = 0
+        # 평균 데이터 출력
+        if elsapsed_time >= standard_time[0] :
+            print("---{}분 동안의 평균 환경 데이터---".format(standard_time[1]))
+            print_json(get_avg(avg_env, cnt))
+            
+            # 다시 측정하기 위해 초기화
+            avg_env = {key: 0 for key in avg_env.keys()}
+            cnt = 0
             start_time = current_time
 
         env_values = RunComputer.get_sensor_data(ds)
         
+        # 환경 데이터 누적 계산
+        for key in keys:
+            avg_env[key] += env_values[key]
+
         cnt += 1
-        avg_env['mars_base_internal_temperature'] += env_values['mars_base_internal_temperature']
-        avg_env['mars_base_external_temperature'] += env_values['mars_base_external_temperature']
-        avg_env['mars_base_internal_humidity'] += env_values['mars_base_internal_humidity']
-        avg_env['mars_base_external_illuminance'] += env_values['mars_base_external_illuminance']
-        avg_env['mars_base_internal_co2'] += env_values['mars_base_internal_co2']
-        avg_env['mars_base_internal_oxygen'] += env_values['mars_base_internal_oxygen']
 
-        print('{')
-        for key, value in env_values.items() :
-            if key == 'mars_base_internal_oxygen' :
-                print('    "{0}": {1}'.format(key, value))
-            else :
-                print('    "{0}": {1},'.format(key, value))
-        print('}')
-
-        print()
+        print_json(env_values)
 
         print("멈추려면 q를 입력하세요...")
-
         print()
         
         time.sleep(5)
--- project1/procedure1/problem8/mars_mission_computer.py
+++ project1/procedure1/problem8/mars_mission_computer.py
@@ -6,6 +6,19 @@ from procedure1.problem7.mars_mission_computer import MissonComputer
 if __name__ == '__main__':
     ds = DummySensor()
     runComputer = MissonComputer()
-    runComputer.get_mission_computer_info()
-    print()
-    runComputer.get_mission_computer_load()
+
+    # 설정 파일 경로
+    setting_file_path = 'procedure1/problem8/setting.txt'
+
+    # 설정
+    setting = []
+
+    # 설정 파일 읽기
+    with open(setting_file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                setting += [item.strip() for item in line.split(',')]
+
+    runComputer.get_mission_computer_info(setting, '운영체제 정보를 불러오는데 오류가 발생했습니다.')
+    runComputer.get_mission_computer_load(setting, '실시간 사용량 정보를 불러오는데 오류가 발생했습니다.')
